6985d9be38b941840f3a0758a8c1be1dbae0f454
junchen.yang <junchen.yang@ke.com>
2021-09-15 15:03:04 +0800

    debug

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,20 +1,34 @@
-import {Command} from '@oclif/command'
-import {pathExists, remove, copySync, writeJsonSync, readJsonSync, outputFileSync} from 'fs-extra';
+import { Command } from '@oclif/command'
+import {
+  pathExists,
+  remove,
+  copySync,
+  writeJsonSync,
+  readJsonSync,
+  outputFileSync,
+} from 'fs-extra'
 import * as path from 'path'
-import * as inquirer from 'inquirer';
-import * as ora from 'ora';
-import * as execa from 'execa';
+import * as inquirer from 'inquirer'
+import * as ora from 'ora'
+import * as execa from 'execa'
 
-const pathsToCheck = [`${path.resolve('./')}/.eslintrc`, `${path.resolve('./')}/.eslintrc.js`, `${path.resolve('./')}/.prettierrc.js`, `${path.resolve('./')}/.prettierrc`] 
+const pathsToCheck = [
+  `${path.resolve('./')}/.eslintrc`,
+  `${path.resolve('./')}/.eslintrc.js`,
+  `${path.resolve('./')}/.prettierrc.js`,
+  `${path.resolve('./')}/.prettierrc`,
+]
 
 // 检查原本是否有配置文件
 const checkConfigFile = async () => {
-  const pro = Promise.all(pathsToCheck.map((path: string) => {
-    return pathExists(path)
-  }))
-  const res = await pro;
-  if (res.some(i => i)) {
-    return 'your project has eslintrc or prettierrc file exists, will you replace it ? Y/n';
+  const pro = Promise.all(
+    pathsToCheck.map((path: string) => {
+      return pathExists(path)
+    }),
+  )
+  const res = await pro
+  if (res.some((i) => i)) {
+    return 'your project has eslintrc or prettierrc file exists, will you replace it ? Y/n'
   }
   return false
 }
@@ -24,100 +38,112 @@ const modifyPackageJson = async () => {
   const packageObj = readJsonSync(`${path.resolve('./')}/package.json`)
   packageObj.scripts = {
     ...(packageObj.scripts || {}),
-    prepare: 'husky install'
+    prepare: 'husky install',
   }
 
   // 配置检查规则
-  packageObj['lint-staged'] =  {
-    "*.{js,jsx,vue,tsx, tx}": [
-      "prettier --write",
-      "eslint --ext .vue,.js,.jsx,.tsx,.ts --fix",
-      "git add"
-    ]
+  packageObj['lint-staged'] = {
+    '*.{js,jsx,vue,tsx,ts,json}': [
+      'prettier --write',
+      'eslint --ext .vue,.js,.jsx,.tsx,.ts --fix',
+      'git add',
+    ],
   }
   writeJsonSync(`${path.resolve('./')}/package.json`, packageObj)
 
   // 初始化husky配置
   await execa.command('npm run prepare')
-  copySync(`${__dirname}/config/pre-commit`, `${path.resolve('./')}/.husky/pre-commit`)
-  console.log('init success');
+  copySync(
+    `${__dirname}/config/pre-commit`,
+    `${path.resolve('./')}/.husky/pre-commit`,
+  )
+  console.log('init success')
 }
 
 // 安装依赖
 const installPackages = async () => {
   const spinner = ora({
     text: 'install eslint',
-    spinner: 'moon',
-    color: 'yellow'
-  }).start();
- 
-  await execa.command('npm i eslint --save-dev')
-  spinner.text = 'install prettier';
-  await execa.command('npm i prettier --save-dev')
+    // spinner: 'moon',
+    color: 'yellow',
+  }).start()
 
-  spinner.text = 'install babel-eslint';
-  await execa.command('npm i babel-eslint --save-dev')
+  // await execa.command('npm i eslint --save-dev')
+  // spinner.text = 'install prettier';
+  // await execa.command('npm i prettier --save-dev')
 
-  spinner.text = 'install eslint-plugin-prettier';
-  await execa.command('npm i eslint-plugin-prettier --save-dev')
+  // spinner.text = 'install babel-eslint';
+  // await execa.command('npm i babel-eslint --save-dev')
 
-  spinner.text = 'install husky';
-  await execa.command('npm i husky --save-dev')
+  // spinner.text = 'install eslint-plugin-prettier';
+  // await execa.command('npm i eslint-plugin-prettier --save-dev')
 
-  spinner.text = 'install lint-staged';
-  await execa.command('npm i lint-staged --save-dev')
+  // spinner.text = 'install husky';
+  // await execa.command('npm i husky --save-dev')
 
-  spinner.text = 'install standard';
-  await execa.command('npm i standard --save-dev')
+  // spinner.text = 'install lint-staged';
+  // await execa.command('npm i lint-staged --save-dev')
 
-  spinner.text = 'install typescript';
-  await execa.command('npm i typescript --save-dev')
+  // spinner.text = 'install typescript';
+  // await execa.command('npm i typescript --save-dev')
 
-  spinner.text = 'install @typescript-eslint/parser';
-  await execa.command('npm i @typescript-eslint/parser --save-dev')
-  spinner.text = 'install @typescript-eslint/eslint-plugin';
-  
-  console.log('install success');
-  spinner.color = 'green';
+  // spinner.text = 'install @typescript-eslint/parser';
+  // await execa.command('npm i @typescript-eslint/parser --save-dev')
+
+  spinner.text = 'install eslint plugins'
+  await execa.command(
+    'npm install --save-dev eslint-plugin-promise eslint-plugin-import eslint-plugin-node eslint-plugin-react',
+  )
+  await execa.command(
+    'npm install eslint-config-standard@14.1.1 eslint-plugin-standard -dev',
+  )
+
+  console.log('install success')
+  spinner.color = 'green'
   setTimeout(() => {
-    spinner.stop();
+    spinner.stop()
   }, 1000)
-    modifyPackageJson()
+  modifyPackageJson()
 }
 
 // 移除原本的eslintrc prettierrc文件，并copy当前配置
 const copyConfig = async (removeFirst?: boolean) => {
   if (removeFirst) {
-    const pro = Promise.all(pathsToCheck.map((path: string) => {
-      return remove(path)
-    }))
-    await pro;
+    const pro = Promise.all(
+      pathsToCheck.map((path: string) => {
+        return remove(path)
+      }),
+    )
+    await pro
   }
   copySync(`${__dirname}/config`, path.resolve('./'))
-  installPackages();
+  installPackages()
 }
 
-
 export class InitCommand extends Command {
-  static description = 'add pretter eslint husky lint-staged for git commit hook'
+  static description =
+    'add pretter eslint husky lint-staged for git commit hook'
+
   async run() {
-    const message = await checkConfigFile();
+    const message = await checkConfigFile()
     if (message) {
-      inquirer.prompt([
-        {
-          type: "confirm",
-          name: 'replace',
-          message
-        }
-      ]).then(res => {
-        if (res.replace) {
-          copyConfig(true);
-        } else {
-          this.log('you choice not to replace, exist the process');
-        }
-      })
-      return;
+      inquirer
+        .prompt([
+          {
+            type: 'confirm',
+            name: 'replace',
+            message,
+          },
+        ])
+        .then((res) => {
+          if (res.replace) {
+            copyConfig(true)
+          } else {
+            this.log('you choice not to replace, exist the process')
+          }
+        })
+      return
     }
-    copyConfig();
+    copyConfig()
   }
 }
